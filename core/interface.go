492f77eaa00dd78b30f487fba7f808f5fa5ef8ed
philip-morlier <philipmorlier@gmail.com>
2021-10-20 08:37:34 -0700

    updated interface to relfect changes to tracer interface

--- core/interface.go
+++ core/interface.go
@@ -66,6 +66,8 @@ type TracerResult interface {
 	CaptureState(pc uint64, op OpCode, gas, cost uint64, scope ScopeContext, rData []byte, depth int, err error)
 	CaptureFault(pc uint64, op OpCode, gas, cost uint64, scope ScopeContext, depth int, err error)
 	CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error)
+	CaptureEnter(typ OpCode, from Address, to Address, input []byte, gas uint64, value *big.Int)
+	CaptureExit(output []byte, gasUsed uint64, err error)
 	Result() (interface{}, error)
 }
 
